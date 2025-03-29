1f67c54399ee153661319f7110bcaf2434dfe72b
Le Xuan Hung <lxh@Les-MacBook-Air.local>
2025-03-29 14:07:19 +0900

    T2

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,6 @@ app.post('/twoequations/solve', async (req, res) => {
     res.json(records);
   });
 
+const PORT = process.env.PORT || 5000;
 
-
-  app.listen(5001, () => console.log('Backend running on http://localhost:5001'));
+app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
